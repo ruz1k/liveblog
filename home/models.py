8c28e8c3d350efc9e3854d1f2cca3b325b8f56e0
ruz1k <romanchev.ar@gmail.com>
2019-07-29 01:50:46 +0300

    Finish (i think)

--- home/models.py
+++ home/models.py
@@ -3,4 +3,5 @@ from django.conf import settings
 
 class HomePage(models.Model):
 	title = models.CharField(max_length=200)
+	image = models.ImageField(upload_to='media/image',null=True)
 	text = models.TextField()
